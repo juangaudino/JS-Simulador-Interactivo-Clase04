a2f0c7ac5d268d11c6d43a03c48d2cb3d6a25c22
Juan Gaudino <juangaudino@gmail.com>
2022-03-06 17:51:59 -0300

    V1.0.0

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
 // Declaración de valores unitarios
 
 const unitarioPAI = 1200;
-const unitarioCPM = 400;
+const unitarioCPM = 200;
 const metroCuadradoFront = 450;
 const metroCuadradoBack = 730;
 
@@ -38,13 +38,15 @@ function calcularPrecio(material, cantidad, metrosCuadrados) {
 
 // Solicitud de datos
 
-let material = prompt('Ingrese el material: PAI, CPM, Front, Back');
+let material = prompt('Ingrese el material: PAI, CPM, Front o Back');
 
 if (material === 'PAI' || material === 'CPM') {
-    cantidad = prompt(`Ingrese la cantidad de ${material}`);
-    console.log(`El precio del material ${material} es de ${calcularPrecio(material, cantidad)}`);
-    alert(`El precio del material ${material} es de ${calcularPrecio(material, cantidad)}`);
-    document.write(`El precio del material ${material} es de ${calcularPrecio(material, cantidad)}`);
+    cantidad = prompt(`Ingrese la cantidad de ${material} a producir`);
+    console.log(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
+    alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
+    document.write("<h2> Cotización de "+ cantidad +" "+ material +"</h2>");
+    document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad,) + "</h4>");
+    // document.write(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
 }
 else if (material === 'Front' || material === 'Back') {
     ancho = prompt(`Ingrese el ancho del material ${material}`);	
@@ -52,12 +54,17 @@ else if (material === 'Front' || material === 'Back') {
     cantidad = prompt(`Ingrese la cantidad de ${material}`);
     let metrosCuadrados = ancho * alto;
     console.log(`Los metros cuadrados del material ${material} son de ${metrosCuadrados}`);
-    console.log(`El precio del material ${material} es de ${calcularPrecio(material, cantidad, metrosCuadrados)}`);
-    alert(`El precio del material ${material} es de ${calcularPrecio(material, cantidad, metrosCuadrados)}`);
-    document.write(`El precio del material ${material} es de ${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+    console.log(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+    alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+    document.write("<h2> Cotización de "+ cantidad +" "+ material +" "+ "de " + ancho + "x" + alto + " metros" +"</h2>");
+    document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad, metrosCuadrados) + "</h4>");
+    // document.write(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+    
 }
 
 // Document Write
+
+
 
 
 
